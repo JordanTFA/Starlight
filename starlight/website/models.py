9fca43d4502af134726c5d36620f151e0214709e
JordanTFA <Jordantfa@hotmail.co.uk>
2017-08-24 14:25:44 +0100

    Added models

--- starlight/website/models.py
+++ starlight/website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+	cat_name = models.CharField(max_length=50)
+
+	def __str__ (self):
+		return self.cat_name
+
+class Photo(models.Model):
+	category = models.ForeignKey(Category, on_delete=models.CASCADE)
+	photo_name = models.CharField(max_length=30)
+
+	def __str__ (self):
+		return '(' + self.category + ') ' + self.photo_name 
