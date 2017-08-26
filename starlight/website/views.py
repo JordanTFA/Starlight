1d0dfb5c33406c7821403b01fb5187ef853be567
JordanTFA <Jordantfa@hotmail.co.uk>
2017-08-26 17:43:27 +0100

    Started work on gallery

--- starlight/website/views.py
+++ starlight/website/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Category
 
 def index(request):
 	return render(request, 'website/index.html', {'context': "context"})
 
 def gallery(request):
-	return render(request, 'website/gallery.html', {'context': "context"})
+
+	all_categories = Category.objects.order_by('cat_name')
+	context = {'all_categories' : all_categories}
+	return render(request, 'website/gallery.html', context)
