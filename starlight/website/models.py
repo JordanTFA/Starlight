a442aa5220ba52052d9c88f6869e58876a372f82
JordanTFA <Jordantfa@hotmail.co.uk>
2017-08-29 21:55:44 +0100

    Minor changes, need to work on image fields

--- starlight/website/models.py
+++ starlight/website/models.py
@@ -3,7 +3,11 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
 	cat_name = models.CharField(max_length=50)
+	cat_photo = models.IntegerField(default=1) # IMPORTANT: This will be changed to an imageField in the future to represent the category photo
 
+	class Meta:
+		verbose_name_plural = "Categories"
+		
 	def __str__ (self):
 		return self.cat_name
 
