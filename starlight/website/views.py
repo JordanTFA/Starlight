0032c577672804b5db0fc5476d4ed64e41cc8caa
JordanTFA <Jordantfa@hotmail.co.uk>
2017-09-05 22:40:16 +0100

    Some cleaning up and optimising
    Next step is to get those photos showing in its page

--- starlight/website/views.py
+++ starlight/website/views.py
@@ -11,9 +11,12 @@ def galleryView(request):
 	context = {'all_categories' : all_categories}
 	return render(request, 'website/gallery.html', context)
 
-def categoryView(request, cat_name):
+def categoryView(request, cat_name_from_url):
 
-	all_photos = Category.objects.order_by('cat_name')	# TODO: needs fixed so that it shows all photos in a category
-	context = {'all_categories' : all_photos}
+	cat = get_object_or_404(Category, cat_name=cat_name_from_url)
+	return render(request, 'website/photos.html', {'cat': cat})
 
-	return render(request, 'website/photos.html', context)
+	#all_photos = Category.objects.order_by('cat_name')	# TODO: needs fixed so that it shows all photos in a category
+	#context = {'all_categories' : all_photos}
+
+	#return render(request, 'website/photos.html', context)
