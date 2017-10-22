a83026bf30099a6eef897f175228bf60f8903496
JordanTFA <Jordantfa@hotmail.co.uk>
2017-10-22 21:07:20 +0100

    Logins and logouts now redirect properly

--- starlight/starlight/settings.py
+++ starlight/starlight/settings.py
@@ -129,3 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
+
+# Authentication
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL  = '/'
